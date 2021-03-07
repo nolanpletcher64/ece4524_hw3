485ea148d3cd01eb34eb93a54fbe5292537346f8
nolanpletcher64 <nolanp@vt.edu>
2021-03-07 17:17:55 -0500

    Removed fitness function, using one from SudokuBoard, set up Population and Solution, need to implement breeding and better mutation

--- Population.py
+++ Population.py
@@ -5,5 +5,44 @@ import numpy as np
 import Solution
 
 class Population:
-    
-    
+    NUMGENERATIONS = 100
+    PERFECTSCORE = 243
+
+    def __init__(self, popsize, elitism, antielitism):
+        self.popsize = popsize
+        self.solutions = []
+        self.elitism = elitism
+        self.mutateProb = .10
+        self.antielitism = antielitism
+
+    def initialize(self):
+        for i in range(self.popsize):
+            self.solutions.append(Solution.Solution())
+
+    def newGeneration(self):
+        # First mutate the current population
+        for solution in self.solutions:
+            solution.mutate()
+
+        # Sort mutated solutions by fitness
+        self.solutions.sort(reverse=True)
+
+        # Build new population comprised of (elitism) number of most fit, and
+        # (antielitism) number of least fit, filling in rest with random solutions
+        newSolutions = self.solutions[-self.antielitism:] + self.solutions[:self.elitism]
+
+        for i in range(self.popsize - len(newSolutions)):
+            newSolutions.append(Solution.Solution())
+
+        self.solutions = sorted(newSolutions, reverse=True)
+
+        # return max fitness value
+        return self.solutions[0].getFitness()
+
+    def toString(self):
+        popStr = ''
+
+        for solution in self.solutions:
+            popStr += solution.toString() + '\n\n'
+
+        return popStr
--- Solution.py
+++ Solution.py
@@ -2,85 +2,28 @@
 # This class contains a genome that specifies a Sudoku board layout
 
 import numpy as np
+import SudokuBoard
 
-class Solution(object):
-    
-    def __init_(self):
-        self.values = np.zeros((9, 9), dtype=int)
+class Solution(SudokuBoard.SudokuBoard):
+    def __init__(self):
+        super( ).__init__( )
+
         self.fitness = 0
-        return
-    
-    def mutation():
-        
-        return
-        
-    def breeding():
-        
-        return
-        
-    def fitnessEval(self):
-        
-        rowCount = np.zeros(9)
-        rowSum = 0
-        colCount = np.zeros(9)
-        colSum = 0
-        sqrCount = np.zeros(9)
-        sqrSum = 0
-        
-        # Iterate through each row in board
-        for i in range(9):
-            
-            # Iterate through each number in the row
-            for j in range(9):
-                
-                # Increment number of occurences of the number
-                rowCount[self.values[i][j] - 1] += 1
-            
-            # Set rowSum fitness
-            
-            
-            # Reset row count
-            rowCount = np.zeros(9)
-            
-            
-        # Iterate through each column in board
-        for i in range(9):
-            
-            # Iterate through each number in column
-            for j in range(9):
-                
-                # Increment number of occurences of the number
-                colCount[self.values[j][i] - 1] += 1
-                
-            # Set colSum fitness
-            
-            
-            # Reset column count
-            colCount = np.zeros(9)
-            
-            
-        # Iterate through each square in board
-        for i in range(0, 9, 3):
-            
-            # Iterate through each number in square
-            for j in range(0, 9, 3):
-                
-                for k in range(3):
-                    
-                    sqrCount[self.values[i+k][j] - 1] += 1
-                    sqrCount[self.values[i+k][j+1] - 1] += 1
-                    sqrCount[self.values[i+k][j+2] - 1] += 1
-                    
-            # Set sqrSum fitness
-            
-                    
-            # Reset square count
-            sqrCount = np.zeros(9)
-        
-        
-        # Calculate total fitness, PERFECTSCORE = 243
-        self.fitness = "Insert formula here"
-        
-        return
+        self.populate()
+
+    def populate(self):
+        for row in range( self.SZ ):
+            self.cells[row] = list(range(self.SZ))
+            np.random.shuffle(self.cells[row])
+
+        self.fitness = self.getFitness()
+
+    def mutate(self):
+        # For now, just repopulate randomly. Change this to do something smart.
+        self.populate()
+        self.fitness = self.getFitness()
+
+    def __lt__(self, other):
+        return self.fitness < other.fitness
     
     
